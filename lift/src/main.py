b8381e9f754d44f2706d1e9c4ba337664853c641
MadEwink <adrien.floriant@telecom-sudparis.eu>
2019-05-17 17:35:55 +0200

    tabs, first version of site
    implemented a javascript tab system to have several tabs with links,
    server interface, etc, and made it runnable

--- lift/src/main.py
+++ lift/src/main.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
-@main.route("/")
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
+
+@app.route("/")
 def index():
-    return "index"
+    return render_template("accueil.html")
