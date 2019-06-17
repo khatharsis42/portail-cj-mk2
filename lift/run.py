ef30e2a7f36aefa29c387eb2eb19b185813b70e7
⛧ <matthias.goffette@telecom-sudparis.eu>
2019-06-17 21:09:20 +0200

    Adds port and listening address

--- lift/run.py
+++ lift/run.py
@@ -2,4 +2,5 @@ from src.main import app
 
 if __name__ == "__main__":
     app.config.from_pyfile("config.py")
-    app.run()
+    app.run(host=app.config["LISTEN_ADDR"], port=app.config["LISTEN_PORT"])
+
